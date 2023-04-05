fb9ac5772fd68ad9e8b40a5c4ff1ad5d501fc065
Heartbeast <heartbeast.studios@gmail.com>
2023-04-05 14:50:14 -0600

    Rigid Bodies + Instancing

--- ship.gd
+++ ship.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const LASER_SCENE = preload("res://laser.tscn")
+
 @export var speed = 100
 
 func _process(delta):
@@ -7,3 +9,8 @@ func _process(delta):
 		position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		var laser = LASER_SCENE.instantiate()
+		var world = get_tree().current_scene
+		world.add_child(laser)
+		laser.position = position
